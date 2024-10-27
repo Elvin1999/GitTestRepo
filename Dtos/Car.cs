01a9f8ffcf9e754c5f227b2ff75475f394b13c09
Elvin <camalzade_elvin@mail.ru>
2024-10-27 10:56:36 +0400

    i added 3 properties

--- Dtos/Car.cs
+++ Dtos/Car.cs
@@ -4,5 +4,8 @@
     {
         public int Id { get; set; }
         public string? Name { get; set; }
+        public string? Model { get; set; }
+        public string? Brand { get; set; }
+        public string? Engine { get; set; }
     }
 }
